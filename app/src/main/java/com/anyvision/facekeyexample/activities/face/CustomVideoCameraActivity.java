38ab85c3f1cccda64f5b7bd8540a321e97de346e
ewertonrichieri <contato.ewertonrichieri@gmail.com>
2020-12-02 11:11:16 -0300

    alterou o txt com o d:

--- app/src/main/java/com/anyvision/facekeyexample/activities/face/CustomVideoCameraActivity.java
+++ app/src/main/java/com/anyvision/facekeyexample/activities/face/CustomVideoCameraActivity.java
@@ -10,7 +10,6 @@
 //import android.util.Log;
 //import android.view.SurfaceHolder;
 //import android.view.SurfaceView;
-//import com.anyvision.facekeyexample.R;
 //import com.anyvision.facekeyexample.activities.CameraPreview;
 //
 //

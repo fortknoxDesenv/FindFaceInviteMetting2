6609c575fa116bfe5fa2ea248805d47d61cf7ebc
ewertonrichieri <contato.ewertonrichieri@gmail.com>
2020-12-02 11:09:28 -0300

    alterou o txt com o d:

--- app/src/main/java/com/anyvision/facekeyexample/activities/face/CustomVideoCameraActivity.java
+++ app/src/main/java/com/anyvision/facekeyexample/activities/face/CustomVideoCameraActivity.java
@@ -13,7 +13,6 @@
 //import com.anyvision.facekeyexample.R;
 //import com.anyvision.facekeyexample.activities.CameraPreview;
 //
-//import java.io.File;
 //import java.io.IOException;
 //
 //public class CustomVideoCameraActivity extends Activity implements SurfaceHolder.Callback {

af7559a03f323bb22d0e97579825f75020d65a56
ewertonrichieri <contato.ewertonrichieri@gmail.com>
2021-03-12 14:54:52 -0300

    Inviting

--- app/src/main/java/com/anyvision/facekeyexample/activities/LoginActivity.java
+++ app/src/main/java/com/anyvision/facekeyexample/activities/LoginActivity.java
@@ -112,7 +112,7 @@ public class LoginActivity extends BaseActivity {
                 }
             }
         });
- //teste
+
         alertDialogBuilder.setNegativeButton("SAIR", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
